c9a21acf7c84077ab5125419996c8b5b1f4b2017
CogentRedTester <cogent.redtester@outlook.com>
2021-01-18 19:28:57 +1030

    updated get-user-input function

--- search-page.lua
+++ search-page.lua
@@ -104,20 +104,26 @@ opt.read_options(o, "search_page")
 -----------------------------------------------
 local counter = 1
 local function get_user_input(funct, options)
-    if options then
-        options = utils.format_json(options)
-        if not options then error("table cannot be converted to json string") ; return end
-    end
+    local name = mp.get_script_name()
+    options = options or {}
+    options.id = options.id or name
+    options.text = options.text or (name.." is requesting user input:")
+
+    local response_string = name.."/__user_input_request/"..counter
+    options.response = response_string
+
+    options = utils.format_json(options)
+    if not options then error("table cannot be converted to json string") ; return end
 
     -- create a callback for user-input to respond to
-    local response_string = mp.get_script_name().."/__user_input_request/"..counter
     counter = counter + 1
     mp.register_script_message(response_string, function(response)
         mp.unregister_script_message(response_string)
-        funct(response)
+        response = utils.parse_json(response)
+        funct(response.input, response.err)
     end)
 
-    mp.commandv("script-message-to", "user_input", "request-user-input", options or "", response_string)
+    mp.commandv("script-message-to", "user_input", "request-user-input", options)
 end
 -----------------------------------------------
 -----------------------------------------------
